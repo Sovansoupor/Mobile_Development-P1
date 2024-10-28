b0db09423fb1ae35a0f3e6a50dc3d75a35f1b2a2
Soupor <sovansouporeng@gmail.com>
2024-10-28 14:41:51 +0700

    Add files via upload

--- W3-S2-Flutter-App/EX-4/main.dart
+++ W3-S2-Flutter-App/EX-4/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({required this.title, super.key});
+  const CustomCard({required this.title,required this.gradient, super.key});
   final String title;
-
+  final Gradient? gradient; 
+  // nullable cus 
+  //final Color color; plain color
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-        child: Column(
-          children: [
+    return 
             Container(
-            padding: const EdgeInsets.all(20),
+            // padding: const EdgeInsets.all(20),
             margin: const EdgeInsets.symmetric(vertical: 20),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(50),
-              color: Colors.blue[100], 
+              //color: color, using plain color
+              // for gradient
+
+              gradient: gradient ?? 
+              const LinearGradient( // in case gradient isnt provided by user, it will use this default value
+              colors: [Color(0xff4e7df5) ,Color.fromARGB(255, 5, 34, 109)]), 
             ),
 
             child:  Center(
@@ -28,17 +32,32 @@ class CustomCard extends StatelessWidget {
                 ),
               ),
             ),
-          )
-          ],   
-    ),
-    ),
-    );
+          );
+          
   }
 }
 
 void main() {
-  runApp(const 
-    CustomCard(title:"Flutter"),
+  runApp( MaterialApp( 
+    home: Container(
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.all(20),
+        child: const Column(
+            children: [
+              CustomCard(title: "FLUTTER", gradient: LinearGradient(
+                colors: [Colors.orange, Colors.deepOrangeAccent],
+              ),),
+              CustomCard(title: "DART", gradient: LinearGradient(
+                colors: [Colors.pink, Colors.black],
+              ),),
+              CustomCard(title: "OOP",gradient: LinearGradient(
+                colors: [Colors.red, Colors.purple],
+              ), ),
+              // CustomCard(title: "OOP",color: Colors.pink), how u pass plain color
+            ],
+        ),
+    ),
 
+  )
   );
 }
